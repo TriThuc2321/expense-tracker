e9a8342388e258d7d94237aa7f9b4c1530cfb029
TriThuc2321 <19522321@gm.uit.edu.vn>
2023-02-14 01:58:41 +0700

    feat: custom button status

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './Button.module.scss';
+import DotSpinner from '../dotSpinner';
 
 interface Props {
     text: string;
-    className: string | null;
     outline: boolean | null;
-    children?: React.ReactNode;
+    status: 'ACTIVE' | 'LOADING' | 'DISABLE';
+    className: string | null;
     onClick: () => void;
+    children?: React.ReactNode;
 }
 
-const Button: React.FC<Props> = ({ text, onClick, outline = false, className = '' }) => {
+const Button: React.FC<Props> = ({ text = '', outline = false, status = 'ACTIVE', className = '', onClick }) => {
     return (
-        <button
+        <div
             onClick={onClick}
-            className={clsx({ [styles.button]: true, [styles.outline]: outline }, `${className}`)}
+            className={clsx(
+                {
+                    [styles.button]: true,
+                    [styles.outline]: outline,
+                    [styles.disable]: status == 'DISABLE',
+                    [styles.loading]: status == 'LOADING',
+                },
+                `${className}`,
+            )}
         >
-            {text}
-        </button>
+            <p>{text}</p>
+            {status == 'LOADING' && <DotSpinner className="ml-2" />}
+        </div>
     );
 };
 
