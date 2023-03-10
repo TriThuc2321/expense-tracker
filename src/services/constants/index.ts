edb10cac18f64bff469b8991297651654dc18dbd
TriThuc2321 <19522321@gm.uit.edu.vn>
2023-03-10 13:35:53 +0700

    Update index.ts

--- src/services/constants/index.ts
+++ src/services/constants/index.ts
@@ -2,4 +2,4 @@ export const BASE_URL = 'http://localhost:7000/api/v1';
 // export const BASE_URL_GRAPHQL = 'http://localhost:8000';
 // export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://localhost:8000/graphql';
 export const BASE_URL_GRAPHQL = 'https://expenser-tracker-graphql.onrender.com';
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-holetex-server.onrender.com/graphql';
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://expenser-tracker-graphql.onrender.com/graphql';
